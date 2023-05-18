dbcc24e4ab350df77d816168e156f92ff4709c0b
nikgub_ <danger8217.nik@gmail.com>
2023-05-18 23:30:33 +0300

    Registered custom WoodType for future usages

---
+++ src/main/java/net/team_prometheus/pyromancer/blocks/ModWoodType.java
@@ -0,0 +1,8 @@
+package net.team_prometheus.pyromancer.blocks;
+
+import net.minecraft.world.level.block.state.properties.WoodType;
+
+public class ModWoodType {
+    WoodType PYROWOOD = WoodType.create("pyrowood");
+    WoodType ROTTEN_FUNGUS = WoodType.create("rotten_fungus");
+}
