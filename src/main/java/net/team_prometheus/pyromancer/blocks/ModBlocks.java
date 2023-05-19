d190f9fc242c907cf2b2bd303ba2cd8dc7f067d2
nikgub_ <danger8217.nik@gmail.com>
2023-05-20 00:43:23 +0300

    More blocks!!

--- src/main/java/net/team_prometheus/pyromancer/blocks/ModBlocks.java
+++ src/main/java/net/team_prometheus/pyromancer/blocks/ModBlocks.java
@@ -25,29 +25,26 @@ public class ModBlocks {
             () -> log(MaterialColor.WOOD, MaterialColor.PODZOL), CreativeModeTab.TAB_BUILDING_BLOCKS);
     public static final RegistryObject<Block> PYROWOOD_PLANKS = registerBlock("pyrowood_planks",
             () -> new Block((BlockBehaviour.Properties.of(Material.WOOD, MaterialColor.WOOD).strength(2.0F, 3.0F).sound(SoundType.WOOD))), CreativeModeTab.TAB_BUILDING_BLOCKS);
-    public static final RegistryObject<Block> PYROWOOD_SIGN = registerBlock("pyrowood_sign",
-            () -> new StandingSignBlock(BlockBehaviour.Properties.of(Material.WOOD).noCollission().strength(1.0F).sound(SoundType.WOOD), ModWoodType.PYROWOOD), CreativeModeTab.TAB_BUILDING_BLOCKS);
-    public static final RegistryObject<Block> PYROWOOD_WALL_SIGN = registerBlock("pyrowood_wall_sign",
-            () -> new WallSignBlock(BlockBehaviour.Properties.of(Material.WOOD).noCollission().strength(1.0F).sound(SoundType.WOOD).lootFrom(PYROWOOD_SIGN), ModWoodType.PYROWOOD), CreativeModeTab.TAB_BUILDING_BLOCKS);
     public static final RegistryObject<Block> PYROWOOD_STAIRS = registerBlock("pyrowood_stairs",
             () -> new StairBlock(PYROWOOD_PLANKS.get().defaultBlockState(), BlockBehaviour.Properties.copy(PYROWOOD_PLANKS.get())), CreativeModeTab.TAB_BUILDING_BLOCKS);
     public static final RegistryObject<Block> PYROWOOD_SLAB = registerBlock("pyrowood_slab",
             () -> new SlabBlock(BlockBehaviour.Properties.copy(PYROWOOD_PLANKS.get())), CreativeModeTab.TAB_BUILDING_BLOCKS);
     //
-    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab){
+
+
+    public static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab){
         RegistryObject<T> output = BLOCKS.register(name, block);
         registerBlockItem(name, output, tab);
         return output;
     }
-    private static <T extends Block> void registerBlockItem(String name, Supplier<T> block, CreativeModeTab tab){
+    public static <T extends Block> void registerBlockItem(String name, Supplier<T> block, CreativeModeTab tab){
         ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
     }
     public static void register(IEventBus bus){
         BLOCKS.register(bus);
     }
-    private static RotatedPillarBlock log(MaterialColor p_50789_, MaterialColor p_50790_) {
-        return new RotatedPillarBlock(BlockBehaviour.Properties.of(Material.NETHER_WOOD, (p_152624_) -> {
-            return p_152624_.getValue(RotatedPillarBlock.AXIS) == Direction.Axis.Y ? p_50789_ : p_50790_;
-        }).strength(2.0F).sound(SoundType.WOOD));
+    public static RotatedPillarBlock log(MaterialColor materialColor, MaterialColor materialColor1) {
+        return new RotatedPillarBlock(BlockBehaviour.Properties.of(Material.NETHER_WOOD, (p_152624_) ->
+                p_152624_.getValue(RotatedPillarBlock.AXIS) == Direction.Axis.Y ? materialColor : materialColor1).strength(2.0F).sound(SoundType.WOOD));
     }
 }
