1c245a2e232edcc97bf6a09777a42cfa3dd34f7d
nikgub_ <danger8217.nik@gmail.com>
2023-05-19 20:44:58 +0300

    More blocks

--- src/main/java/net/team_prometheus/pyromancer/blocks/ModBlocks.java
+++ src/main/java/net/team_prometheus/pyromancer/blocks/ModBlocks.java
@@ -4,10 +4,7 @@ import net.minecraft.core.Direction;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.RotatedPillarBlock;
-import net.minecraft.world.level.block.SoundType;
-import net.minecraft.world.level.block.StandingSignBlock;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.material.MaterialColor;
@@ -20,6 +17,7 @@ import net.team_prometheus.pyromancer.items.ModItems;
 
 import java.util.function.Supplier;
 
+@SuppressWarnings("deprecation")
 public class ModBlocks {
     public static DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, PyromancerMod.MOD_ID);
     //pyrowood
@@ -27,22 +25,28 @@ public class ModBlocks {
             () -> log(MaterialColor.WOOD, MaterialColor.PODZOL), CreativeModeTab.TAB_BUILDING_BLOCKS);
     public static final RegistryObject<Block> PYROWOOD_PLANKS = registerBlock("pyrowood_planks",
             () -> new Block((BlockBehaviour.Properties.of(Material.WOOD, MaterialColor.WOOD).strength(2.0F, 3.0F).sound(SoundType.WOOD))), CreativeModeTab.TAB_BUILDING_BLOCKS);
-    public static final RegistryObject<Block> PYROWOOD_SIGN = registerBlock("oak_sign",
+    public static final RegistryObject<Block> PYROWOOD_SIGN = registerBlock("pyrowood_sign",
             () -> new StandingSignBlock(BlockBehaviour.Properties.of(Material.WOOD).noCollission().strength(1.0F).sound(SoundType.WOOD), ModWoodType.PYROWOOD), CreativeModeTab.TAB_BUILDING_BLOCKS);
+    public static final RegistryObject<Block> PYROWOOD_WALL_SIGN = registerBlock("pyrowood_wall_sign",
+            () -> new WallSignBlock(BlockBehaviour.Properties.of(Material.WOOD).noCollission().strength(1.0F).sound(SoundType.WOOD).lootFrom(PYROWOOD_SIGN), ModWoodType.PYROWOOD), CreativeModeTab.TAB_BUILDING_BLOCKS);
+    public static final RegistryObject<Block> PYROWOOD_STAIRS = registerBlock("pyrowood_stairs",
+            () -> new StairBlock(PYROWOOD_PLANKS.get().defaultBlockState(), BlockBehaviour.Properties.copy(PYROWOOD_PLANKS.get())), CreativeModeTab.TAB_BUILDING_BLOCKS);
+    public static final RegistryObject<Block> PYROWOOD_SLAB = registerBlock("pyrowood_slab",
+            () -> new SlabBlock(BlockBehaviour.Properties.copy(PYROWOOD_PLANKS.get())), CreativeModeTab.TAB_BUILDING_BLOCKS);
     //
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab){
         RegistryObject<T> output = BLOCKS.register(name, block);
         registerBlockItem(name, output, tab);
         return output;
     }
-    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, Supplier<T> block, CreativeModeTab tab){
-        return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
+    private static <T extends Block> void registerBlockItem(String name, Supplier<T> block, CreativeModeTab tab){
+        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
     }
     public static void register(IEventBus bus){
         BLOCKS.register(bus);
     }
     private static RotatedPillarBlock log(MaterialColor p_50789_, MaterialColor p_50790_) {
-        return new RotatedPillarBlock(BlockBehaviour.Properties.of(Material.WOOD, (p_152624_) -> {
+        return new RotatedPillarBlock(BlockBehaviour.Properties.of(Material.NETHER_WOOD, (p_152624_) -> {
             return p_152624_.getValue(RotatedPillarBlock.AXIS) == Direction.Axis.Y ? p_50789_ : p_50790_;
         }).strength(2.0F).sound(SoundType.WOOD));
     }
