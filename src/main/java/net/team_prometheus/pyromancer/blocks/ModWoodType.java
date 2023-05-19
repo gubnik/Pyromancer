f3457daf370f988363cb370554d144d9201d8afe
nikgub_ <danger8217.nik@gmail.com>
2023-05-19 16:41:48 +0300

    Fixed improper registration

--- src/main/java/net/team_prometheus/pyromancer/blocks/ModWoodType.java
+++ src/main/java/net/team_prometheus/pyromancer/blocks/ModWoodType.java
@@ -3,6 +3,6 @@ package net.team_prometheus.pyromancer.blocks;
 import net.minecraft.world.level.block.state.properties.WoodType;
 
 public class ModWoodType {
-    WoodType PYROWOOD = WoodType.create("pyrowood");
-    WoodType ROTTEN_FUNGUS = WoodType.create("rotten_fungus");
+    public static final WoodType PYROWOOD = WoodType.create("pyrowood");
+    public static final WoodType ROTTEN_FUNGUS = WoodType.create("rotten_fungus");
 }
