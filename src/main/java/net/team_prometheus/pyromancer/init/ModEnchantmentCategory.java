b53e310511905c8adbbab6b0c0e53b1ca888edc6
nikgub_ <danger8217.nik@gmail.com>
2023-05-21 02:13:39 +0300

    Expanded enchantment category list for future usages

--- src/main/java/net/team_prometheus/pyromancer/init/ModEnchantmentCategory.java
+++ src/main/java/net/team_prometheus/pyromancer/init/ModEnchantmentCategory.java
@@ -1,8 +1,12 @@
 package net.team_prometheus.pyromancer.init;
 
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
++import net.team_prometheus.pyromancer.items.BlazingJournal;
 import net.team_prometheus.pyromancer.items.MaceItem;
+import net.team_prometheus.pyromancer.items.PyromancyItem;
 
 public class ModEnchantmentCategory {
     public static EnchantmentCategory MACES = EnchantmentCategory.create("MACES", MaceItem::isMace);
+    public static EnchantmentCategory PYROMANCIES = EnchantmentCategory.create("PYROMANCIES", PyromancyItem::isPyromancy);
+    public static EnchantmentCategory BLAZING_JOURNAL = EnchantmentCategory.create("JOURNAL", BlazingJournal::isJournal);
 }
