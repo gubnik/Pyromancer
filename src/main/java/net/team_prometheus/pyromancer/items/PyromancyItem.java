82a2849d05618220d5579e65613a9b4325543583
nikgub_ <danger8217.nik@gmail.com>
2023-05-21 02:09:03 +0300

    Added PyromancyItem class

---
+++ src/main/java/net/team_prometheus/pyromancer/items/PyromancyItem.java
@@ -0,0 +1,35 @@
+package net.team_prometheus.pyromancer.items;
+
+import com.google.common.collect.ImmutableListMultimap;
+import com.google.common.collect.Multimap;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.item.*;
+import net.team_prometheus.pyromancer.PyromancerMod;
+import net.team_prometheus.pyromancer.init.ModAttributes;
+import org.jetbrains.annotations.NotNull;
+
+@SuppressWarnings("deprecation")
+public class PyromancyItem extends Item implements Vanishable {
+    private final int blazeConsumption;
+    private final Multimap<Attribute, AttributeModifier> defaultModifiers;
+    public PyromancyItem(int blazeCost, int uses, Properties properties) {
+        super(properties.stacksTo(1).defaultDurability(uses));
+        this.blazeConsumption = blazeCost;
+        ImmutableListMultimap.Builder<Attribute, AttributeModifier> builder = ImmutableListMultimap.builder();
+        builder.putAll(super.getDefaultAttributeModifiers(EquipmentSlot.MAINHAND));
+        builder.put(ModAttributes.BLAZE_CONSUMPTION.get(), new AttributeModifier(PyromancerMod.BLAZE_CONSUMPTION_UUID, "Weapon modifier", this.blazeConsumption, AttributeModifier.Operation.ADDITION));
+        this.defaultModifiers = builder.build();
+    }
+    @Override
+    public @NotNull Multimap<Attribute, AttributeModifier> getDefaultAttributeModifiers(@NotNull EquipmentSlot slot) {
+        return slot == EquipmentSlot.MAINHAND ? this.defaultModifiers : super.getDefaultAttributeModifiers(slot);
+    }
+    public int getCost(){
+        return this.blazeConsumption;
+    }
+    public static boolean isPyromancy(Item item){
+        return(item instanceof PyromancyItem);
+    }
+}
