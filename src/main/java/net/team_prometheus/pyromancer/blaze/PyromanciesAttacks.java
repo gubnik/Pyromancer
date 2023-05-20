39b22a6db6fa777c45f9c999edabc12ddc54683d
nikgub_ <danger8217.nik@gmail.com>
2023-05-21 02:10:38 +0300

    Added PyromanciesAttacks, for a while it will lack blaze consumption

--- src/main/java/net/team_prometheus/pyromancer/blaze/PyromanciesAttacks.java
+++ src/main/java/net/team_prometheus/pyromancer/blaze/PyromanciesAttacks.java
@@ -1,10 +1,7 @@
 package net.team_prometheus.pyromancer.blaze;
 
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.entity.projectile.Fireball;
-import net.minecraft.world.entity.projectile.SmallFireball;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
