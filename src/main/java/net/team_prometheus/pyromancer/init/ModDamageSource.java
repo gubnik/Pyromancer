645b41fea50d8a1458bd254317bae8b268534923
nikgub_ <danger8217.nik@gmail.com>
2023-05-23 03:51:54 +0300

    Added custom damage sources

---
+++ src/main/java/net/team_prometheus/pyromancer/init/ModDamageSource.java
@@ -0,0 +1,15 @@
+package net.team_prometheus.pyromancer.init;
+
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.damagesource.IndirectEntityDamageSource;
+import net.minecraft.world.entity.Entity;
+import net.team_prometheus.pyromancer.entity.projectiles.SizzlingHandFireball;
+import org.jetbrains.annotations.Nullable;
+
+public class ModDamageSource {
+    public static DamageSource INCINERATION = (new DamageSource("incineration")).setIsFire().bypassEnchantments();
+    public static DamageSource ROT = (new DamageSource("rot")).bypassArmor().bypassEnchantments();
+    public static DamageSource sizzlingHandFireball(SizzlingHandFireball fireball, @Nullable Entity entity){
+        return entity == null ? (new IndirectEntityDamageSource("onFire", fireball, fireball)).setIsFire().setProjectile() : (new IndirectEntityDamageSource("fireball", fireball, entity)).setIsFire().setProjectile();
+    }
+}
