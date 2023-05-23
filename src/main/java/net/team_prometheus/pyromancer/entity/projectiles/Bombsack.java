bcf5d520fc6ab5317f8f5f297f1fec766950bade
nikgub_ <danger8217.nik@gmail.com>
2023-05-23 23:32:14 +0300

    Added bombsack

---
+++ src/main/java/net/team_prometheus/pyromancer/entity/projectiles/Bombsack.java
@@ -0,0 +1,68 @@
+package net.team_prometheus.pyromancer.entity.projectiles;
+
+import net.minecraft.core.particles.ItemParticleOption;
+import net.minecraft.core.particles.ParticleOptions;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.monster.Blaze;
+import net.minecraft.world.entity.projectile.ThrowableItemProjectile;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Explosion;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.EntityHitResult;
+import net.minecraft.world.phys.HitResult;
+import net.minecraftforge.network.PlayMessages;
+import net.team_prometheus.pyromancer.entity.ModEntities;
+import net.team_prometheus.pyromancer.init.ModDamageSource;
+import net.team_prometheus.pyromancer.items.ModItems;
+import org.jetbrains.annotations.NotNull;
+
+public class Bombsack extends ThrowableItemProjectile {
+    public Bombsack(PlayMessages.SpawnEntity packet, Level world) {
+        super(ModEntities.BOMBSACK.get(), world);
+    }
+    public Bombsack(EntityType<? extends ThrowableItemProjectile> entityType, Level level) {
+        super(ModEntities.BOMBSACK.get(), level);
+    }
+    @Override
+    protected @NotNull Item getDefaultItem() {
+        return ModItems.BOMBSACK.get();
+    }
+    private ParticleOptions getParticle() {
+        ItemStack itemstack = this.getItemRaw();
+        return (itemstack.isEmpty() ? ParticleTypes.SMOKE : new ItemParticleOption(ParticleTypes.ITEM, itemstack));
+    }
+    @Override
+    public void handleEntityEvent(byte p_37402_) {
+        if (p_37402_ == 3) {
+            ParticleOptions particleoptions = this.getParticle();
+            for(int i = 0; i < 8; ++i) {
+                this.level.addParticle(particleoptions, this.getX(), this.getY(), this.getZ(), 0.0D, 0.0D, 0.0D);
+            }
+        }
+
+    }
+    @Override
+    protected void onHitEntity(@NotNull EntityHitResult entityHitResult) {
+        super.onHitEntity(entityHitResult);
+        Entity entity = entityHitResult.getEntity();
+        int i = entity instanceof Blaze ? 3 : 0;
+        entity.hurt(DamageSource.thrown(this, this.getOwner()), (float)i);
+    }
+    @Override
+    protected void onHit(@NotNull HitResult hitResult) {
+        super.onHit(hitResult);
+        if (!this.level.isClientSide) {
+            this.level.broadcastEntityEvent(this, (byte)3);
+            this.discard();
+            collisionEffect(this, this.level);
+        }
+
+    }
+    public static void collisionEffect(Bombsack bombsack, Level level){
+        level.explode(bombsack, ModDamageSource.BOMBSACK, null, bombsack.getX(), bombsack.getY(), bombsack.getZ(), 1.3f, false, Explosion.BlockInteraction.NONE);
+    }
+}
