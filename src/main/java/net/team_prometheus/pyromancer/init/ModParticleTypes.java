86fa73d56ec779fc1449dccb6b86140189ae783a
nikgub_ <danger8217.nik@gmail.com>
2023-05-23 23:31:47 +0300

    Added custom particles

---
+++ src/main/java/net/team_prometheus/pyromancer/init/ModParticleTypes.java
@@ -0,0 +1,15 @@
+package net.team_prometheus.pyromancer.init;
+
+import net.minecraft.core.particles.ParticleType;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.core.particles.SimpleParticleType;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.team_prometheus.pyromancer.PyromancerMod;
+
+public class ModParticleTypes {
+    public static final DeferredRegister<ParticleType<?>> PARTICLES = DeferredRegister.create(ForgeRegistries.PARTICLE_TYPES, PyromancerMod.MOD_ID);
+    public static final RegistryObject<ParticleType<SimpleParticleType>> ROT = PARTICLES.register("rot_particle",
+            ()-> new SimpleParticleType(true));
+}
---
+++ src/main/java/net/team_prometheus/pyromancer/init/ModParticles.java
@@ -0,0 +1,15 @@
+package net.team_prometheus.pyromancer.init;
+
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RegisterParticleProvidersEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.team_prometheus.pyromancer.particles.RotParticle;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public class ModParticles {
+    @SubscribeEvent
+    public static void registerParticles(RegisterParticleProvidersEvent event) {
+        event.register(ModParticleTypes.ROT.get(), RotParticle::provider);
+    }
+}
