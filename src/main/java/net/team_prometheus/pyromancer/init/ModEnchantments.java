ee136bf9b954d6e32d30f312be0614c65f6f0a40
nikgub_ <danger8217.nik@gmail.com>
2023-06-21 02:51:54 +0300

    A complete clusterfuck, do not touch unless properly high

--- src/main/java/net/team_prometheus/pyromancer/init/ModEnchantments.java
+++ src/main/java/net/team_prometheus/pyromancer/init/ModEnchantments.java
@@ -1,6 +1,9 @@
 package net.team_prometheus.pyromancer.init;
 
 import net.minecraft.core.Direction;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
@@ -10,6 +13,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.team_prometheus.pyromancer.PyromancerMod;
+import org.jetbrains.annotations.NotNull;
 
 public class ModEnchantments {
     public static DeferredRegister<Enchantment> ENCHANTMENTS = DeferredRegister.create(ForgeRegistries.ENCHANTMENTS, PyromancerMod.MOD_ID);
@@ -18,17 +22,34 @@ public class ModEnchantments {
         @Override
         public int getMaxLevel() {return 5;}
         @Override
-        public void doPostAttack(LivingEntity attacker, Entity target, int level){
-            float modifier = (float) Math.pow(
-                    Math.pow(attacker.getDeltaMovement().get(Direction.Axis.X), 2) +
-                    Math.pow(attacker.getDeltaMovement().get(Direction.Axis.Y), 2) +
-                    Math.pow(attacker.getDeltaMovement().get(Direction.Axis.Z), 2)
-            , 0.5);
-            target.hurt(DamageSource.GENERIC.bypassInvul(), modifier);
+        public void doPostAttack(@NotNull LivingEntity attacker, @NotNull Entity target, int level){
+            double modifier = Math.sqrt(
+                    Math.pow(attacker.getDeltaMovement().x(), 2) +
+                    Math.pow(attacker.getDeltaMovement().z(), 2)
+            );
+            if(attacker instanceof ServerPlayer serverPlayer){
+                serverPlayer.sendSystemMessage(Component.literal("x: " + String.valueOf(attacker.getDeltaMovement().x())));
+                serverPlayer.sendSystemMessage(Component.literal("z: " + String.valueOf(attacker.getDeltaMovement().z())));
+                serverPlayer.sendSystemMessage(Component.literal("speed: " + String.valueOf(attacker.getSpeed())));
+            }
+            target.hurt(DamageSource.GENERIC.bypassInvul(), (float) modifier);
         }
         @Override
         public boolean isAllowedOnBooks() {
             return true;
+        }
+    });
+    public static RegistryObject<Enchantment> STURDINESS = ENCHANTMENTS.register("sturdiness",
+            () -> new Enchantment(Enchantment.Rarity.COMMON, ModEnchantmentCategory.MACES, new EquipmentSlot[]{}) {
+        @Override
+        public int getMaxLevel(){return 5;
+        }
+        @Override
+        public void doPostAttack(@NotNull LivingEntity attacker, @NotNull Entity target, int level){
+            if(target instanceof LivingEntity target_living){
+                target_living.hurt(DamageSource.GENERIC.bypassInvul().bypassArmor(),
+                        (float) (target_living.getArmorValue()*0.2));
+            }
         }
     });
 }
