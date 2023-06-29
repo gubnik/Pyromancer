e671f513cf4b40df14c3c12ef7bb6ddee2d2f480
gubnik <danger8217.nik@gmail.com>
2023-06-30 00:49:14 +0300

    FUCKING DID IT!!!

--- src/main/java/net/team_prometheus/pyromancer/entity/EntityUtils.java
+++ src/main/java/net/team_prometheus/pyromancer/entity/EntityUtils.java
@@ -4,6 +4,7 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.Projectile;
 import net.team_prometheus.pyromancer.entity.projectiles.PyromancyFireballProjectile;
+import net.team_prometheus.pyromancer.init.ModAttributes;
 import net.team_prometheus.pyromancer.items.ItemUtils;
 
 import javax.annotation.Nullable;
@@ -12,7 +13,7 @@ public class EntityUtils {
     public static void shootPyromancyFireballProjectile(PyromancyFireballProjectile projectile, float speed, float inaccuracy, int timer, @Nullable Player shooter){
         assert shooter != null;
         projectile.setOwner(shooter);
-        projectile.damage = ItemUtils.getPyromancyDamage(shooter);
+        projectile.damage = (int) shooter.getAttributeValue(ModAttributes.PYROMANCY_DAMAGE.get());
         projectile.maxLifetime = timer;
         projectile.setPos(shooter.getX(), shooter.getEyeY() - 0.3, shooter.getZ());
         projectile.shoot(shooter.getLookAngle().x, shooter.getLookAngle().y, shooter.getLookAngle().z, speed, inaccuracy);
